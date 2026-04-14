b6781090ce092d619cd0350935fd3cdf336aa1b5
Satyam Kumar <189542961+ImSatyamK@users.noreply.github.com>
2026-04-14 19:02:13 +0530

    Make basic interface

---
+++ assembly_endgame/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <h3>Assembly: Endagame</h3>
+            <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+        </header>
+    )
+}
